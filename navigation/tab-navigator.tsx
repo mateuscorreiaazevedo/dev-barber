68b8d262d1aee5e925d582609bfab1ccbe067a19
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-11-10 16:07:55 -0300

    config biome

--- navigation/tab-navigator.tsx
+++ navigation/tab-navigator.tsx
@@ -1,22 +1,23 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StackScreenProps } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import type { StackScreenProps } from '@react-navigation/stack'
 
-import { RootStackParamList } from '.';
-import { HeaderButton } from '../components/HeaderButton';
-import { TabBarIcon } from '../components/TabBarIcon';
-import One from '../screens/one';
-import Two from '../screens/two';
+import type { RootStackParamList } from '.'
+import { HeaderButton } from '../components/HeaderButton'
+import { TabBarIcon } from '../components/TabBarIcon'
+import One from '../screens/one'
+import Two from '../screens/two'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
-type Props = StackScreenProps<RootStackParamList, 'TabNavigator'>;
+type Props = StackScreenProps<RootStackParamList, 'TabNavigator'>
 
 export default function TabLayout({ navigation }: Props) {
   return (
     <Tab.Navigator
       screenOptions={{
         tabBarActiveTintColor: 'black',
-      }}>
+      }}
+    >
       <Tab.Screen
         name="One"
         component={One}
@@ -35,5 +36,5 @@ export default function TabLayout({ navigation }: Props) {
         }}
       />
     </Tab.Navigator>
-  );
+  )
 }
